f3d67e46674e0994bc12583bb6ac3ee6d2f3855c
ManithMam <100299120+ManithMam@users.noreply.github.com>
2023-03-19 17:09:36 +0100

    Add connection to db and add User schema

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,23 @@
-import * as fs from 'fs';
+import  express  from "express";
+import mongoose from "mongoose";
 
-fs;
+const app = express()
+
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+
+let db = null
+const url = `mongodb://127.0.0.1:27017/NoteTakingApp`
+mongoose.connect(url)
+.then(() => {
+    console.log('Connecting to database NoteTakingApp...')    
+})
+
+
+app.listen(3000)
+console.log('Server listening on Port 3000')
+
+
+
+
+
