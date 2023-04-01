6d54453fc7f424c6a66a1956255f5d37620add09
ManithMam <100299120+ManithMam@users.noreply.github.com>
2023-04-01 17:41:20 +0200

    Add specific user get route

--- backend/src/Routes/UserRoutes.ts
+++ backend/src/Routes/UserRoutes.ts
@@ -1,5 +1,5 @@
 import express from "express"
-import UserController, {default as userController} from "../Controller/User/UserController.js"
+import UserController from "../Controller/User/UserController.js"
 import User from "../Model/User.js"
 
 const userRouter = express.Router()
@@ -8,12 +8,23 @@ userRouter.post('/users', async (req, res) => {
     try{
         const username: string = req.body.username
         const password: string = req.body.password
-        const newUser = userController.createUser(username, password)
+        const newUser = UserController.createUser(username, password)
         res.json(newUser).status(200)
     }
     catch(err){
 
     }
+})
+
+userRouter.get('/users/:username', async (req, res, next) => {
+    try{
+        const username: string = req.params.username
+        const user = await User.find({username: username})
+        res.send(user).status(200)
+    }
+    catch(err){
+        next(err)
+    }   
 })
 
 userRouter.patch('/users', async (req, res) => {
@@ -31,8 +42,8 @@ userRouter.patch('/users', async (req, res) => {
 
 userRouter.patch('/users/deleteNote', async(req, res) => {
     try{
-        const noteId = req.body.noteId
-        const username = req.body.username
+        const noteId: string = req.body.noteId
+        const username: string = req.body.username
 
         UserController.deleteUserNote(noteId, username)
         res.send("deleted note").status(200)
