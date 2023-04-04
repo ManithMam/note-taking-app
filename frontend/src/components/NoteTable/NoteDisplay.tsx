39103dcf41849f71d10ff739cb5a63091cab1950
ManithMam <100299120+ManithMam@users.noreply.github.com>
2023-04-04 11:35:50 +0200

    Delete notes in db through clicking on the button next to the note

--- frontend/src/components/NoteTable/NoteDisplay.tsx
+++ frontend/src/components/NoteTable/NoteDisplay.tsx
@@ -4,13 +4,21 @@ export default function NoteDisplay(props: {notes: note}){
 
     const {notes} = props
 
+    function deleteNote(){
+        fetch('http://localhost:8080/notes', {
+            headers: { 'Content-Type': 'application/json' },
+            method: "DELETE",
+            mode: "cors",
+            body: JSON.stringify({id: notes.stringId})
+        })
+    }
+
     const display = (
         <div key={notes.stringId}>
             <ul>{notes.content}</ul>
-            <input type="button"/>
+            <input type="button" onClick={deleteNote}/>
         </div>
-    )
-    
+    )    
     
     return(
         <div>
