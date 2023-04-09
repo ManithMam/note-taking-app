a7f0b39ec71ccc2752ccc7ebd5270a06120be1e2
ManithMam <100299120+ManithMam@users.noreply.github.com>
2023-04-09 17:09:54 +0200

    #11 Implement alert when an empty note is created

--- frontend/src/components/NoteTable/NoteTaking.tsx
+++ frontend/src/components/NoteTable/NoteTaking.tsx
@@ -8,6 +8,10 @@ export default function NoteTaking(props: {setNotes: Function, setError: Functio
     const input = useRef<HTMLInputElement>(null)    
 
      function sendNotes(){
+        if(input.current?.value === ""){
+            return alert("Please enter a value.")
+        }
+
         fetch('http://localhost:8080/notes', {
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({content: input.current?.value}),
